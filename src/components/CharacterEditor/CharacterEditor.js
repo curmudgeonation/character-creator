36b559d96a63248d590d854eb50a1c1adacf4dc4
Justin Anderson <justin@watchworddigital.com>
2021-04-25 15:53:58 -0700

    Finish layout, overflow, perspective

--- src/components/CharacterEditor/CharacterEditor.js
+++ src/components/CharacterEditor/CharacterEditor.js
@@ -31,8 +31,8 @@ function App() {
   return (
     <>
     <main className={styles.characterEditor}>
+      <div className={styles.perspectivePanel}/>
       <MaxWidthWrapper className={styles.maxWidthWrapper}>
-        <div className={styles.perspectivePanel}/>
         <header className={styles.header}>
           <h1 className={styles.title}>Create your Character</h1>
           <p className={styles.description}>
